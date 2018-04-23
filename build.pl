1e608ff35660eb55956d74db9ed8b627681d9ca6
Christian Brunn <Christian.Brunn@malighting.de>
2018-04-23 15:00:08 +0200

    chang linux download for libs (use mounted network drive from wbb.malighting.de)

--- build.pl
+++ build.pl
@@ -158,20 +158,15 @@ sub Build
         {
             #network path was not available -> try to access path via network-drive (normally the case for build server)
             my $netSrcFile = ToOsPath("S:/Projekte/webdaemon_libs/win_libs.zip");
-            copy($netSrcFile, $dstFile) or die "Copy Failed: $!";
+            copy($netSrcFile, $dstFile) or die "Copy Failed: $!\n";
         }
     }
     elsif ($osname eq 'linux')
     {
-        #for linux we also copy the file from our \\LUMEN\MA server
-        $srcFile = "file://malighting/ma/Software/Projekte/webdaemon_libs/lin_libs.zip";
+        #for linux we also copy the file from our MA server (wbb.malighting.de) -> ma dir from server should be mounted to /media/ma_server/
+        $srcFile = "/media/ma_server/Software/Projekte/webdaemon_libs/lin_libs.zip";
         print ("> copy($srcFile, $dstFile)\n");
-        unless(copy($srcFile, $dstFile))
-        {
-            #network path was not available -> try to access path via network-drive (normally the case for build server)
-            my $netSrcFile = ToOsPath("S:/Projekte/webdaemon_libs/lin_libs.zip");
-            copy($netSrcFile, $dstFile) or die "Copy Failed: $!";
-        }
+        die "Copy Failed: $!\n" unless($remote->copy($srcFile, $dstFile));
     }
     else
     {
